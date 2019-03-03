40f70111257a11f7d0ab6f9a42406ba92130847b
Nate Tan <nktan93@gmail.com>
2019-03-03 23:19:05 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+require 'pry'
 
+array = ["nate", "karl", "chin", "shannon"]
+
+def my_collect(array)
+  i = 0
+  new_array = []
+  while i < array.length
+    new_array << yield(array[i])
+    i = i + 1
+  end
+  
+  new_array
+end
+
+my_collect(array) do |name|
+  name.capitalize
+end
 
